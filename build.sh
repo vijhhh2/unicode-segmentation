b38d612748f94a118e2becbd9758b173a4f10d12
vijay <vijay.mangapoti@gmail.com>
2024-02-12 23:49:20 +0530

    Updated readme, build file and dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
 
+# Check if at least one argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: You need to provide version number. Ex: $0 0.1.0"
+    exit 1
+fi
+
+echo "Vestion number is: $1"
+
+file_path="dist/package.json"
+readme_file_path="readme.md"
+readme_file_dest_path="dist/readme.md"
+package_json="{
+    \"name\": \"unicode_segmentation\",
+    \"version\": \"$1\",
+    \"description\": \"This library provides node binding for rust package Unicode Segmentation Crate\",
+    \"author\": {
+        \"name\": \"Vijay\",
+        \"email\": \"vijay.mangapoti@gmail.com\",
+        \"url\": \"https://github.com/vijhhh2\"
+    },
+    \"repository\": {
+        \"url\": \"git+https://github.com/vijhhh2/unicode-segmentation.git\"
+    },
+    \"keywords\": [
+        \"unicode\",
+        \"unicode_segmentation\",
+        \"grapheme\"
+    ],
+    \"files\": [
+        \"index.node\",
+        \"lib.js\",
+        \"lib.d.ts\"
+    ],
+    \"module\": \"lib.js\",
+    \"main\": \"lib.js\",
+    \"types\": \"lib.d.ts\",
+    \"license\": \"MIT\"
+}"
+
+# Create a release build
 export TSLINK_BUILD=true
 nj-cli build --release
+
+cat <<EOF > "$file_path"
+$package_json
+EOF
+
+echo "written $file_path"
+
+# Copy the readme file to dist
+cp "$readme_file_path" "$readme_file_dest_path"
+
+# publish the package to npm
+cd ./dist || exit
+npm publish --access public
--- readme.md
+++ readme.md
@@ -3,11 +3,32 @@
 ##### This library provides node binding for rust package [Unicode Segmentation Crate](https://crates.io/crates/unicode-segmentation)
 
 ## Installation
-```bash
+```shell
 npm install -s unicode-segmentation
 ```
 
 ## Usage
 ```typescript
+import { graphemes } from "unicode_segmentation";
 
+const stringToSplit = "Lorem 😂😂 ipsum 🕵️‍♂️dolor adipiscing😇😇🤙 elit, sed do eiusmod🥰 tempor 😤😤🏳️‍🌈."
+
+const result = graphemes(stringToSplit)
+
+console.log(result);
+/* Output
+[
+  'L',  'o',  'r',  'e',   'm', ' ', '😂', '😂',
+  ' ',  'i',  'p',  's',   'u', 'm', ' ',  '🕵️‍♂️',
+  'd',  'o',  'l',  'o',   'r', ' ', 'a',  'd',
+  'i',  'p',  'i',  's',   'c', 'i', 'n',  'g',
+  '😇', '😇', '🤙', ' ',   'e', 'l', 'i',  't',
+  ',',  ' ',  's',  'e',   'd', ' ', 'd',  'o',
+  ' ',  'e',  'i',  'u',   's', 'm', 'o',  'd',
+  '🥰', ' ',  't',  'e',   'm', 'p', 'o',  'r',
+  ' ',  '😤', '😤', '🏳️‍🌈', '.'
+]
+*/
 ```
+
+This library exposes all functions from the [Unicode Segmentation Crate](https://crates.io/crates/unicode-segmentation). For more detailed documentation please visit this [link](https://unicode-rs.github.io/unicode-segmentation/unicode_segmentation/trait.UnicodeSegmentation.html) 
